8f7d5b51c9d358fc594d02b02a43b988c680c4af
Kevin Lee <kevinlee@rflabskkbookpro.attlocal.net>
2021-01-16 12:55:54 -0500

    graph added

--- client/src/containers/LifeDesignSummary.js
+++ client/src/containers/LifeDesignSummary.js
@@ -3,6 +3,8 @@ import { useUserStore } from "../context/UserContext";
 import { useObserver } from "mobx-react";
 import Insight from "../api/Insight";
 
+import { Bar, Doughnut } from "react-chartjs-2";
+
 import { Magic } from "magic-sdk";
 import Loading from "./Loading";
 const magic = new Magic(process.env.REACT_APP_MAGIC_API_KEY);
@@ -10,6 +12,28 @@ const magic = new Magic(process.env.REACT_APP_MAGIC_API_KEY);
 const LifeDesignSummary = () => {
   const userStore = useUserStore();
   const [did, setDid] = React.useState(null);
+  const [chartData, setChartData] = React.useState({
+    labels: ["Health", "Work", "Play", "Love"],
+    datasets: [
+      {
+        label: "Distribution",
+        data: [0, 0, 0, 0],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  });
 
   useEffect(() => {
     async function logsInit() {
@@ -17,16 +41,37 @@ const LifeDesignSummary = () => {
       setDid(generateIdToken);
       const insight = new Insight(generateIdToken);
       let summary = await insight.getSummary();
-      console.log(summary);
-      console.log(summary.health + summary.work + summary.play + summary.love);
       await userStore.setTotalHoursLogged(
         summary.health + summary.work + summary.play + summary.love
       );
       await userStore.setLifeDesignSummary(summary);
-      console.log(
-        parseFloat(userStore.lifeDesignSummary.health) /
-          parseFloat(userStore.totalHoursLogged)
-      );
+      setChartData({
+        ...chartData,
+        datasets: [
+          {
+            label: "Life Design Summary",
+            data: [
+              userStore.lifeDesignSummary.health.toFixed(2),
+              userStore.lifeDesignSummary.work.toFixed(2),
+              userStore.lifeDesignSummary.play.toFixed(2),
+              userStore.lifeDesignSummary.love.toFixed(2),
+            ],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      });
     }
     logsInit();
   }, []);
@@ -34,70 +79,8 @@ const LifeDesignSummary = () => {
   return useObserver(() => (
     <>
       {userStore.lifeDesignSummary ? (
-        <div className="home-box">
-          <table className="summary-table">
-            <thead className="message-table-head">
-              <tr className="message-table-column">
-                <th className="message-table-header">Category</th>
-                <th className="message-table-header">Distribution</th>
-                <th className="message-table-header">Total (hrs)</th>
-              </tr>
-            </thead>
-            <tbody className="message-table-body">
-              <tr className="message-table-card">
-                <th className="message-table-cell">Health</th>
-                <th className="message-table-cell">
-                  {Math.round(
-                    (userStore.lifeDesignSummary.health /
-                      userStore.totalHoursLogged) *
-                      100
-                  ) + "%"}
-                </th>
-                <th className="message-table-cell">
-                  {userStore.lifeDesignSummary.health}
-                </th>
-              </tr>
-              <tr className="message-table-card">
-                <th className="message-table-cell">Work</th>
-                <th className="message-table-cell">
-                  {Math.round(
-                    (userStore.lifeDesignSummary.work /
-                      userStore.totalHoursLogged) *
-                      100
-                  ) + "%"}
-                </th>
-                <th className="message-table-cell">
-                  {userStore.lifeDesignSummary.work}
-                </th>
-              </tr>
-              <tr className="message-table-card">
-                <th className="message-table-cell">Play</th>
-                <th className="message-table-cell">
-                  {Math.round(
-                    (userStore.lifeDesignSummary.play /
-                      userStore.totalHoursLogged) *
-                      100
-                  ) + "%"}
-                </th>
-                <th className="message-table-cell">
-                  {userStore.lifeDesignSummary.play}
-                </th>
-              </tr>
-              <tr className="message-table-card">
-                <th className="message-table-cell">Love</th>
-                <th className="message-table-cell">
-                  {Math.round(
-                    (userStore.lifeDesignSummary.love /
-                      userStore.totalHoursLogged) *
-                      100
-                  ) + "%"}
-                </th>
-                <th className="message-table-cell">
-                  {userStore.lifeDesignSummary.love}
-                </th>
-              </tr>
-            </tbody>
-          </table>
+        <div className="chart-box">
+          <Doughnut data={chartData} />
         </div>
       ) : (
         <Loading />
